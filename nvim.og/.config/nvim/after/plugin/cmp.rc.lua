a461df52a55ea65772590ea118d91b2fd261d5f1
Oskaras <oskaras@plek.co>
2025-04-15 14:02:43 +0200

    nvim cmp prompts with highlight colors

--- nvim.og/.config/nvim/after/plugin/cmp.rc.lua
+++ nvim.og/.config/nvim/after/plugin/cmp.rc.lua
@@ -74,7 +74,17 @@ cmp.setup({
 		end, { "i", "s" }),
 	},
 	formatting = {
-		format = lspkind.cmp_format({ with_text = false, maxwidth = 50 }),
+		format = function(entry, item)
+			local color_item = require("nvim-highlight-colors").format(entry, { kind = item.kind })
+			item = require("lspkind").cmp_format({
+				-- any lspkind format settings here
+			})(entry, item)
+			if color_item.abbr_hl_group then
+				item.kind_hl_group = color_item.abbr_hl_group
+				item.kind = color_item.abbr
+			end
+			return item
+		end,
 	},
 })
 
